f32a66f38befd6cc3a3dd6b68ca0824dfe4ee9ff
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-10-26 20:56:58 +0800

    Create export_inp.m
    输出管网模型的应用

---
+++ application/export_inp.m
@@ -0,0 +1,14 @@
+% analysis for the chosen scenario
+clear;clc;close all;
+load('Scenario.mat');
+input_net_name1 = 'GWSL_pump1.inp';
+input_net_name2 = 'GWSL_4.inp';
+input_rr_file_name = 'GWSL_4_RR_IX.txt';
+MC = MC_simulation(input_rr_file_name,input_net_name2);
+MC.MC_Nmax = 500;
+MC.ChosenScenarioIndex = 5;
+MC.Damage_info = DamageScenario;
+MC.pre_damage_analysis
+MC.export_inp('MC5_nopump.inp')
+MC.delete
+
