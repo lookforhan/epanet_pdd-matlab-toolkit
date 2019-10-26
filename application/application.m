d802cf9b52eedca4a7d4e7ec2e5e10fee8b5db11
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-10-26 20:57:31 +0800

    Create application.m

---
+++ application/application.m
@@ -0,0 +1,32 @@
+% application
+% the MC_simulation.m is used to simulate the network model with pump and
+% without pump.
+input_net_name1 = 'GWSL_pump1.inp';
+input_net_name2 = 'GWSL_4.inp';
+input_rr_file_name = 'GWSL_4_RR_IX.txt';
+MC = MC_simulation(input_rr_file_name,input_net_name1);
+MC.MC_Nmax = 500;
+MC.pre_analysis
+MC.random_value
+MC.generate_damage_probability
+MC.generate_damage_info
+DamageScenario = MC.Damage_info;
+save('Scenario.mat','DamageScenario');
+MC.analysis
+MC.post_analysis
+WithPump = saveobj(MC);
+save('resultWithPump.mat','WithPump')
+MC.delete
+
+% result analysis
+
+
+MC = MC_simulation(input_rr_file_name,input_net_name2);
+MC.MC_Nmax = 500;
+MC.pre_analysis
+MC.Damage_info = DamageScenario;
+MC.analysis
+MC.post_analysis
+WithoutPump = saveobj(MC);
+save('resultWithoutPump.mat','WithoutPump');
+MC.delete
