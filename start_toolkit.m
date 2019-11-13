4aa5b470e83ba573cffe8d5addd1ab069d5d3df7
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-11-13 10:12:21 +0800

    Update start_toolkit.m

--- start_toolkit.m
+++ start_toolkit.m
@@ -34,8 +34,8 @@ addpath(genpath(pwd));
 disp(pwd)
 disp('EPANET_PDD-MATLAB Toolkit Paths Loaded.');
 % path('E:\hanzhao\the_study_of_leak_model_in_epanet',path);
-path('E:\hanzhao\a-comparison-of-2-technologies-that-analyze-resilience\lib\damageNet\',path);
-path('E:\hanzhao\PlotPub\lib',path);
+% path('E:\hanzhao\a-comparison-of-2-technologies-that-analyze-resilience\lib\damageNet\',path);
+% path('E:\hanzhao\PlotPub\lib',path);
 % path('E:\hanzhao\adjustBoxDim',path);
 %------------- END OF CODE --------------
 
